590b89b5cf94c3681ebde294fd910e5a6eaf2ed3
Renyn <r.awesomelee@gmail.com>
2024-07-16 10:47:33 -1000

    Imported gradient img

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      background:{
+        'gradient-img':"url('/assets/images/Circle.png')",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
