e6b0d18e2caa2cfbbb71c4321c93d4450cd146b2
Celeste Layne <layne.celeste@gmail.com>
2024-07-22 11:20:06 -0400

    reorder the components on the chat route

--- src/components/ThreeColumnLayout.tsx
+++ src/components/ThreeColumnLayout.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const ThreeColumnLayout = ({ leftChildren, middleChildren, rightChildren }) => (
   <div className="flex flex-col justify-between md:flex-row md:justify-between w-full min-h-screen">
+    {/* Side Navigation */}
+    <div className="md:w-300 w-full p-16">{leftChildren}</div>
+    
     {/* Chat History */}
-    <div className="md:w-2/5 w-full bg-orange-400 pt-16">{leftChildren}</div>
+    <div className="flex flex-col md:w-300 w-full min-h-screen p-16">{middleChildren}</div>
+    
     {/* Chat */}
-    <div className="flex flex-col justify-center md:w-2/5 w-full min-h-screen bg-orange-500 pt-16">{middleChildren}</div>
-    {/* User Profile */}
-    <div className="md:w-2/5 w-full bg-orange-700 pt-16">{rightChildren}</div>
+    <div className="flex-1 w-full p-16">{rightChildren}</div>
   </div>
 );
 
