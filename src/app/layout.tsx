6d03dd8004fe54c9adb87515885c521dd426ef2e
Celeste Layne <layne.celeste@gmail.com>
2024-07-22 11:16:34 -0400

    add mona sans to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,41 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
-import { inter } from "@/app/ui/fonts"; 
+// import { inter } from "@/app/ui/fonts"; 
+import localFont from 'next/font/local'
 
 // best practice to add to the top level component
 import "./globals.css";
+
+const monaSans = localFont({
+  src: [
+    {
+      path: '../styles/Mona-Sans/WOFF-2/MonaSans-Regular.woff2',
+      weight: '400',
+      style: 'bold',
+    },
+    {
+      path: '../styles/Mona-Sans/WOFF-2/MonaSans-ExtraBold.woff2',
+      weight: '400',
+      style: 'extra bold',
+    },
+    {
+      path: '../styles/Mona-Sans/WOFF-2/MonaSans-Medium.woff2',
+      weight: '400',
+      style: 'medium',
+    },
+    {
+      path: '../styles/Mona-Sans/WOFF-2/MonaSans-Regular.woff2',
+      weight: '400',
+      style: 'regular',
+    },
+    {
+      path: '../styles/Mona-Sans/WOFF-2/MonaSans-SemiBold.woff2',
+      weight: '400',
+      style: 'semi bold',
+    },
+  ], 
+  display: 'swap',
+})
 
 export const metadata = {
   title: "Meet Your AI Business Buddy",
@@ -18,7 +50,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} antialiased`}>
+        <body className={`${monaSans.className} antialiased bg-regal-blue text-white`}>
           <Navbar />
           {children}
         </body>
