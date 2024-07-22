5c4c80435cd75d6734f2032c6a50cd0a7c106739
Celeste Layne <layne.celeste@gmail.com>
2024-07-22 14:43:25 -0400

    adjust column spacing

--- src/components/ThreeColumnLayout.tsx
+++ src/components/ThreeColumnLayout.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const ThreeColumnLayout = ({ leftChildren, middleChildren, rightChildren }) => (
   <div className="flex flex-col justify-between md:flex-row md:justify-between w-full min-h-screen">
     {/* Side Navigation */}
-    <div className="md:w-300 w-full p-16 text-white">{leftChildren}</div>
+    <div className="md:w-300 w-full mt-16 p-2 text-white">{leftChildren}</div>
     
     {/* Chat History */}
-    <div className="flex flex-col md:w-300 w-full min-h-screen p-16 text-white">{middleChildren}</div>
+    <div className="flex flex-col md:w-300 w-full min-h-screen mt-16 p-2 text-white">{middleChildren}</div>
     
     {/* Chat */}
-    <div className="flex-1 w-full p-16 text-black">{rightChildren}</div>
+    <div className="flex-1 w-full mt-16 p-2 text-black">{rightChildren}</div>
   </div>
 );
 
