6e702b58a69ac6efcd62a3b0b776faf5053718d3
Celeste Layne <layne.celeste@gmail.com>
2024-07-22 14:08:29 -0400

    update text colors for each column

--- src/components/ThreeColumnLayout.tsx
+++ src/components/ThreeColumnLayout.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const ThreeColumnLayout = ({ leftChildren, middleChildren, rightChildren }) => (
   <div className="flex flex-col justify-between md:flex-row md:justify-between w-full min-h-screen">
     {/* Side Navigation */}
-    <div className="md:w-300 w-full p-16">{leftChildren}</div>
+    <div className="md:w-300 w-full p-16 text-white">{leftChildren}</div>
     
     {/* Chat History */}
-    <div className="flex flex-col md:w-300 w-full min-h-screen p-16">{middleChildren}</div>
+    <div className="flex flex-col md:w-300 w-full min-h-screen p-16 text-white">{middleChildren}</div>
     
     {/* Chat */}
-    <div className="flex-1 w-full p-16">{rightChildren}</div>
+    <div className="flex-1 w-full p-16 text-black">{rightChildren}</div>
   </div>
 );
 
