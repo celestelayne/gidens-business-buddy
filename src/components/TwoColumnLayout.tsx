8b0102297224222df71c1c905baa25d62181db88
Celeste Layne <layne.celeste@gmail.com>
2024-07-23 21:48:03 -0400

    rename layout component for chat segment

---
+++ src/components/TwoColumnLayout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TwoColumnLayout = ({ leftChildren, rightChildren }) => (
+  <div className="flex flex-col justify-between md:flex-row md:justify-between w-full">
+  
+    {/* Chat History */}
+    <div className="flex flex-col w-1/6 p-2 text-white">{leftChildren}</div>
+    
+    {/* Chat */}
+    <div className="flex-1 w-full p-2 text-black">{rightChildren}</div>
+  </div>
+);
+
+export default TwoColumnLayout;
