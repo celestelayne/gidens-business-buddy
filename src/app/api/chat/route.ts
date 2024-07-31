b69c71f1d5b97c5d8599dc19b0a38f3a853c7d67
Celeste Layne <layne.celeste@gmail.com>
2024-07-31 00:33:47 -0400

    create vector store from pinecone index retrieve and generate

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,18 +1,24 @@
-import { NextResponse } from "next/server";
-import { ChatOpenAI } from "@langchain/openai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import dotenv from "dotenv";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
 
 import {
     ChatPromptTemplate,
     MessagesPlaceholder,
 } from "@langchain/core/prompts";
-
 import { HumanMessage, AIMessage } from "@langchain/core/messages";
-import { ChatMessageHistory } from "langchain/stores/message/in_memory";
 
-import { BufferMemory } from "langchain/memory";
-import { ConversationChain } from "langchain/chains";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
 
 import { currentUser } from '@clerk/nextjs/server';
+import { NextResponse } from "next/server";
+
+
+
+dotenv.config({ path: `.env.local` });
 
 export async function POST(request: Request) {
 
@@ -26,39 +32,78 @@ export async function POST(request: Request) {
             throw new Error("No input provided.");
         }
 
+        /*
+            ========== PINECONE ==========
+        */
+
+        const pinecone = new Pinecone({
+            apiKey: process.env.PINECONE_API_KEY as string,
+        });
+    
+        const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX || "");
+
+        // Create vector store from existing Pinecone index
+        const vectorStore = await PineconeStore.fromExistingIndex(
+            new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
+            { pineconeIndex }
+        );
+
+        /*
+            ========== LANCHAIN / OPENAI ==========
+        */
+
         const model = new ChatOpenAI({
             model: "gpt-3.5-turbo",
             openAIApiKey: process.env.OPENAI_API_KEY as string,
-            temperature: 0,
+            temperature: 0.3
         })
 
         const prompt = ChatPromptTemplate.fromMessages([
-            ["system", `You are a small business advisor called Gidens having a conversation with a small business owner. Please call me by my first name.`],
+            [
+                "system", `Answer the questions based on the below context: 
+                
+                {context}`
+            ],
             new MessagesPlaceholder("chat_history"),
             ["human", "{input}"],
         ]);
 
-        const pastMessages = [
+        const promptSearchQuery = ChatPromptTemplate.fromMessages([
+            new MessagesPlaceholder("chat_history"),
+            ["user", "{input}"],
+            [
+                "user",
+                "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation",
+            ],
+        ]);
+
+        const chatHistory = [
             new HumanMessage(`Hello, I'm ${user.firstName}.`),
             new AIMessage(`Nice to meet you, ${user.firstName}!`),
         ];
 
-        const chatPromptMemory = new BufferMemory({ 
-            memoryKey: "chat_history",
-            returnMessages: true,
-            chatHistory: new ChatMessageHistory(pastMessages),
+        const historyAwareRetrieverChain = await createHistoryAwareRetriever({
+            llm: model,
+            retriever: vectorStore.asRetriever(),
+            rephrasePrompt: promptSearchQuery,
         });
 
-        const chain = new ConversationChain({
+        const combineDocsChain = await createStuffDocumentsChain({
             llm: model,
-            prompt,
-            verbose: true,
-            memory: chatPromptMemory,
+            prompt: prompt
+        });
+
+        const chain = await createRetrievalChain({
+            retriever: historyAwareRetrieverChain,
+            combineDocsChain,
         });
         
-        const response = await chain.invoke({ input });    
+        const response = await chain.invoke({ 
+            chat_history: chatHistory,
+            input: input,
+        });
         
-        console.log({ response })
+        console.log(response.answer)
 
         return NextResponse.json({ output: response }, { status: 200 });
     } catch (e) {
