396e7c5ac5b0977ffd1f276dbf6c9f12ecbf0ac5
Celeste Layne <layne.celeste@gmail.com>
2024-08-01 17:04:40 -0400

    link logo to landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,24 +11,26 @@ export default function Navbar() {
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="bg-regal-blue relative flex h-32 items-center justify-between">
           <div className="flex flex-1 items-center justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <Image
-                width={0}
-                height={0}
-                sizes="100vw"
-                className="block h-8 w-auto lg:hidden rounded-lg"
-                src="/gidens-fullname-logo-02.png"
-                alt="gidens"
-              />
-              <Image
-                width={0}
-                height={0}
-                sizes="100vw"
-                className="hidden h-8 w-auto lg:block rounded-lg"
-                src="/gidens-fullname-logo-02.png"
-                alt="gidens"
-              />
-            </div>
+            <Link href="/">
+              <div className="flex flex-shrink-0 items-center">
+                <Image
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  className="block h-8 w-auto lg:hidden rounded-lg"
+                  src="/gidens-fullname-logo-02.png"
+                  alt="gidens"
+                />
+                <Image
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  className="hidden h-8 w-auto lg:block rounded-lg"
+                  src="/gidens-fullname-logo-02.png"
+                  alt="gidens"
+                />
+              </div>
+            </Link>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             {userId ? (
