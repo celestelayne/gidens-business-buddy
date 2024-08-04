4066f0a1b52946b5d5deb78b62a4fa42ab770849
Celeste Layne <layne.celeste@gmail.com>
2024-08-04 10:21:20 -0400

    update call to action button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
 
@@ -27,8 +28,27 @@ export default async function Home() {
         </h4>
 
         <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-          <Link href="/sign-up" className="rounded-md bg-yellow px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</Link>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
+          <SignedIn>
+            <Link
+              href="/chat"
+              className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold"
+            >
+              Chat
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>
+              <button className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold">
+                Sign In
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <Link
+            href="/"
+            className="px-4 py-2 rounded-full text-[#131316] text-sm font-semibold bg-[#F7F7F8]"
+          >
+            Learn more
+          </Link>
         </div>
 
     </main>
