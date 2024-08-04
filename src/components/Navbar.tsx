e4bf52bcdf6ac95435490654c15c8ab4f7e62c34
Celeste Layne <layne.celeste@gmail.com>
2024-08-04 10:23:33 -0400

    update user profile mode from modal to dedicated page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
+import { 
+  UserButton, 
+  SignInButton, 
+  SignedOut, 
+  SignedIn
+} from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
-  const { userId } = auth();
-  
   return (
     <div className="w-full fixed top-0 z-10">
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
@@ -33,18 +35,15 @@ export default function Navbar() {
             </Link>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {userId ? (
-              <UserButton afterSignOutUrl="/" />
-            ) : (
-              <>
-                <Link
-                  href="/sign-in"
-                  className="rounded-md bg-gray-800 py-2 px-3 text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  Sign In
-                </Link>
-              </>
-            )}
+              <SignedIn>
+                <UserButton 
+                  showName
+                  userProfileMode='navigation'
+                  userProfileUrl='/user-profile'/>
+              </SignedIn>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
           </div>
         </div>
       </div>
