2718caedbee29d48d4143a7cfa244f2ceb5e6f0d
Celeste Layne <layne.celeste@gmail.com>
2024-09-26 17:05:59 -0400

    update logo to black fullname

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     <div className="w-full fixed top-0 z-10">
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-32 items-center justify-between">
-          {/* <div className="flex flex-1 items-center justify-start">
+          <div className="flex flex-1 items-center justify-start">
             <Link href="/">
               <div className="flex flex-shrink-0 items-center">
                 <Image
@@ -20,20 +20,20 @@ export default function Navbar() {
                   height={0}
                   sizes="100vw"
                   className="block h-8 w-auto lg:hidden rounded-lg"
-                  src="/gidens-fullname-logo-02.png"
-                  alt="gidens"
+                  src="/gidens-fullname-logo.png"
+                  alt="gidens fullname logo"
                 />
                 <Image
                   width={0}
                   height={0}
                   sizes="100vw"
                   className="hidden h-8 w-auto lg:block rounded-lg"
-                  src="/gidens-fullname-logo-02.png"
-                  alt="gidens"
+                  src="/gidens-fullname-logo.png"
+                  alt="gidens fullname logo"
                 />
               </div>
             </Link>
-          </div> */}
+          </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <SignedIn>
                 <UserButton 
