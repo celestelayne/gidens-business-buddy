dad1b03f2e67023c37efe465f4aee9fc5fbfe480
Celeste Layne <layne.celeste@gmail.com>
2024-10-26 23:45:11 -0400

    resolve peer dependency for eslint-config-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,13 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import { FlatCompat } from "@eslint/eslintrc";
-import { fixupConfigRules } from "@eslint/compat";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: eslint.configs.recommended,
-    allConfig: eslint.configs.all
-});
 
-const config = [...compat.extends("next/core-web-vitals"), {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    rules: {},
-}]
-
-const configArray = [
-    ...fixupConfigRules(config),
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    { ignores: [".next/*"] },
-]
-
-export default configArray;
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
